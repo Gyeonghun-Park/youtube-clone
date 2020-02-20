7847bae8eb865bf3dfe2eec385679bffb4a1f6b4
Gyeonghun-Park <52763483+Gyeonghun-Park@users.noreply.github.com>
2020-02-21 04:51:59 +0900

    Add router

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from home!");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,4 +20,6 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
