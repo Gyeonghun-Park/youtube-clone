790f192dba2211a6b55fb697ca5b8d5752b53fb5
Gyeonghun-Park <52763483+Gyeonghun-Park@users.noreply.github.com>
2020-03-09 00:13:14 -0400

    Facebook Log In

--- passport.js
+++ passport.js
@@ -15,9 +15,7 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `https://b8d54a2e.ngrok.io/${routes.facebookCallback}`,
-      profileFields: ["id", "displayName", "photos", "email"],
-      scope: ["public_profile", "email"]
+      callbackURL: `http://localhost:4000${routes.githubCallback}`
     },
     githubLoginCallback
   )
@@ -28,7 +26,9 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `http://localhost:4000${routes.facebookCallback}`
+      callbackURL: `https://b8d54a2e.ngrok.io${routes.facebookCallback}`,
+      profileFields: ["id", "displayName", "photos", "email"],
+      scope: ["public_profile", "email"]
     },
     facebookLoginCallback
   )
